255d4ae0630c6e76202944fa9956ff3db9f7922c
seunggyun-jeong <jskyun98@gmail.com>
2022-11-14 00:13:22 +0900

    [FEAT] Input TextField

--- good_habit/lib/SetCutDownModeView.dart
+++ good_habit/lib/SetCutDownModeView.dart
@@ -15,9 +15,41 @@ class _SetCutDownModeState extends State<SetCutDownMode> {
           title: const Text("절연모드 설정"),
           backgroundColor: Colors.pink,
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Text("절연모드 설정"),
+        body: Center(
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      "하루 할당량 설정",
+                      style: TextStyle(
+                        fontFamily: 'Agrro',
+                        fontSize: 50,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: '개수 입력',
+                        hintText: '하루의 다짐을 입력하세요!',
+                        labelStyle: TextStyle(color: Colors.black),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(width: 1, color: Colors.black),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(width: 1, color: Colors.black),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                      ),
+                    )
+                  ]),
+            ),
+          ),
         ));
   }
 }
